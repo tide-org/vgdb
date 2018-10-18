db86a370063ab030cd9ecffa3b1cdf9923beef10
Willem van Ketwich <willvk@gmail.com>
2018-10-18 17:20:22 +1100

    restore methods

--- autoload/conque_term/conque.py
+++ autoload/conque_term/conque.py
@@ -416,6 +416,12 @@ class Conque:
             else:
                 self.tabstops.append(False)
 
+    def idle(self):
+        pass
+
+    def resume(self):
+        pass
+
     def close(self):
         self.proc.close()
 
