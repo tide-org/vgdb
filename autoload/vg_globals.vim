51f953e253cbe89410bace5320fcabf0ba309fae
Willem van Ketwich <willvk@gmail.com>
2018-12-08 19:50:30 +1100

    remove need to vg_filtered_buffers

--- autoload/vg_globals.vim
+++ autoload/vg_globals.vim
@@ -6,7 +6,6 @@ function! vg_globals#source_globals()
     let g:vg_last_register_result = []
     let g:vg_binary_loaded = 0
     let g:vg_symbols_loaded = 0
-    let g:vg_filtered_buffers = ['vg_breakpoints', 'vg_disassembly']
     let g:vg_remote_target = 0
     let g:vg_py = ''
     let g:vg_current_frame_address = ''
--- autoload/vgdb/filter.py
+++ autoload/vgdb/filter.py
@@ -1,12 +1,26 @@
 import vim
 import importlib
 import sys
+import os
+from os import listdir
+from os.path import isfile, join
+
+filtered_buffers_list = []
 
 def filter_lines_for_buffer(lines, buffer_name):
-    filtered_buffers = vim.eval('g:vg_filtered_buffers')
-    if buffer_name.lower() in filtered_buffers:
+    get_filtered_buffers_list()
+    if buffer_name.lower() in filtered_buffers_list:
         lines = call_filter_class(lines, buffer_name)
     return lines
+
+def get_filtered_buffers_list():
+    if len(filtered_buffers_list) == 0:
+        script_path = os.path.dirname(os.path.realpath(__file__))
+        filters_path = os.path.join(script_path, "filters")
+        filter_files = [f for f in listdir(filters_path) if isfile(join(filters_path, f))]
+        for filter_file in filter_files:
+            if filter_file[-3:].lower() == ".py" and filter_file.lower() != "__init__.py":
+                filtered_buffers_list.append(filter_file[:-3])
 
 def call_filter_class(lines, filter_name):
     filter_module = "filters." + filter_name
