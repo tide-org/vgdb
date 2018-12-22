bf4a8f7852e4143fd4bcf62b2811f808c375c581
Willem van Ketwich <willvk@gmail.com>
2018-12-22 22:44:38 +1100

    add tests, fix actionable dict to use callbacks

--- autoload/vgdb/actionable_dict.py
+++ autoload/vgdb/actionable_dict.py
@@ -1,24 +1,26 @@
 class ActionableDict(dict):
 
     parent = None
+    callback = None
 
-    def __init__(self, initial_dict, parent = None):
+    def __init__(self, initial_dict, callback = None, parent = None):
         self.parent = parent
+        self.callback = callback
         for key, value in initial_dict.items():
             if isinstance(value, dict):
-                initial_dict[key] = ActionableDict(value, self)
+                initial_dict[key] = ActionableDict(value, self.callback, self)
         super(ActionableDict, self).__init__(initial_dict)
 
     def __setitem__(self, item, value):
         if isinstance(value, dict):
-            _value = ActionableDict(value, self)
+            _value = ActionableDict(value, self.callback, self)
         else:
             _value = value
-        #print("You are changing the value of {} to {}!!".format(item, _value))
-        self._traverse_parents()
         super(ActionableDict, self).__setitem__(item, _value)
+        if self.callback != None:
+            self.callback(self.__traverse_parents(), _value)
 
-    def _traverse_parents(self):
+    def __traverse_parents(self):
         test_item = self
         parent_keys = [ next(iter(self)) ]
         while(hasattr(test_item, 'parent') and test_item.parent != None):
@@ -26,4 +28,4 @@ class ActionableDict(dict):
                 if value == test_item:
                     parent_keys.append(key)
             test_item = test_item.parent
-        #print("parent keys: " + str(parent_keys[::-1]))
+        return parent_keys[::-1]
--- tests/nose_tests/test_actionable_dict.py
+++ tests/nose_tests/test_actionable_dict.py
@@ -16,7 +16,12 @@ sys.path.insert(0, vgdb_filters_dir)
 sys.path.insert(0, vgdb_functions_dir)
 
 from nose import with_setup
+from nose.tools import nottest
 from actionable_dict import ActionableDict
+
+@nottest
+def callback_test(parent_dict, value):
+    print("value changed: " + str(value) + " with keys: " + str(parent_dict))
 
 class TestActionableDict():
 
@@ -25,5 +30,15 @@ class TestActionableDict():
         pass
 
     def test_can_add_to_dict(self):
-        test_dict = ActionableDict( { 'b': { 'c': 3 } })
-        assert test_dict['b']['c'] == 3
+        self.test_dict = ActionableDict( { 'b': { 'c': 3 } })
+        self.test_dict['b']['c'] = 6
+        assert self.test_dict['b']['c'] == 6
+
+    def test_can_set_callback(self):
+        self.test_dict = ActionableDict( { 'b': { 'c': 3 } }, callback_test)
+        assert self.test_dict.callback != None
+
+    def test_can_call_callback(self):
+        self.test_dict = ActionableDict( { 'b': { 'c': 3 } }, callback_test)
+        self.test_dict['b']['c'] = 5
+        assert self.test_dict.callback != None
