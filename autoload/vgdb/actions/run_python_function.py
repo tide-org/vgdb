84e38d40647deaf04c0084a1d9c1ae6a45b1a33c
Willem van Ketwich <willvk@gmail.com>
2019-02-06 10:49:59 +1100

    refactoring

--- autoload/vgdb/actions/run_python_function.py
+++ autoload/vgdb/actions/run_python_function.py
@@ -7,23 +7,38 @@ import interpolate as Interpolate
 
 class run_python_function(action_base):
 
+    _command_item = {}
+    _buffer_name = {}
+    _command_args = {}
+    _function_file = ''
+    _function_name = ''
+    _input_args = {}
+    _set_on_return = ''
+    _functions_path = ''
+
     def run(self, command_item, buffer_name='', command_args={}):
-        function_file = command_item["function_file"]
-        function_name = command_item["function_name"]
-        input_args = command_item.get("input_args", {})
-        set_on_return = command_item.get("set_on_return", None)
-        functions_path = PathHelpers.resolve_plugin_path('functions')
-        if functions_path not in sys.path:
-            sys.path.insert(0, functions_path)
-        function_module_name = function_file.replace(".py", "")
+        self.__set_locals(command_item, buffer_name, command_args)
+        if self._functions_path not in sys.path:
+            sys.path.insert(0, self._functions_path)
+        function_module_name = self._function_file.replace(".py", "")
         function_module = importlib.import_module(function_module_name)
-        function = getattr(sys.modules[function_module_name], function_name)
-        interpolated_input_args = self.get_interpolated_args(command_item)
-        if command_args:
-            interpolated_input_args["command_args"] = command_args
+        function = getattr(sys.modules[function_module_name], self._function_name)
+        interpolated_input_args = self.get_interpolated_args(self._command_item)
+        if self._command_args:
+            interpolated_input_args["command_args"] = self._command_args
         function_result = function(**interpolated_input_args)
-        if set_on_return:
-            Config().get()["variables"][set_on_return] = function_result
+        if self._set_on_return:
+            Config().get()["variables"][self._set_on_return] = function_result
+
+    def __set_locals(self, command_item, buffer_name, command_args):
+        self._command_item = command_item
+        self._buffer_name = buffer_name
+        self._command_args = command_args
+        self._function_file = self._command_item["function_file"]
+        self._function_name = self._command_item["function_name"]
+        self._input_args = self._command_item.get("input_args", {})
+        self._set_on_return = self._command_item.get("set_on_return", None)
+        self._functions_path = PathHelpers.resolve_plugin_path('functions')
 
     def get_interpolated_args(self, command_item):
         input_args = command_item.get("input_args", {})
