7b4b6ba2a7c604aa04fa664a041fd49a223563d1
Willem van Ketwich <willvk@gmail.com>
2019-02-06 10:43:56 +1100

    refactor

--- autoload/vgdb/actions/run_vim_function.py
+++ autoload/vgdb/actions/run_vim_function.py
@@ -1,6 +1,5 @@
-import vim
 import os
-from config import Config
+import vim
 from action_base import action_base
 import path_helpers as PathHelpers
 import interpolate as Interpolate
@@ -10,7 +9,6 @@ class run_vim_function(action_base):
     def run(self, command_item, buffer_name=''):
         function_name = command_item["function_name"]
         function_file = function_name.split('#')[0] + ".vim"
-        functions_path = Config().get()["settings"]["plugins"]["functions_path"]
         resolved_functions_path = PathHelpers.resolve_plugin_path("functions")
         function_file_path = os.path.join(resolved_functions_path, function_file)
         vim.command("source " + function_file_path)
