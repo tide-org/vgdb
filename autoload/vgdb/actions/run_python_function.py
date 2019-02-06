1c7d9e44d98c4619f91ec2065b849bd49816fc24
Willem van Ketwich <willvk@gmail.com>
2019-02-06 11:02:42 +1100

    refactor

--- autoload/vgdb/actions/run_python_function.py
+++ autoload/vgdb/actions/run_python_function.py
@@ -15,23 +15,35 @@ class run_python_function(action_base):
     _input_args = {}
     _set_on_return = ''
     _functions_path = ''
+    _function_module_name = ''
+    _function_module = None
+    _function = None
+    _interpolated_input_args = {}
 
     def run(self, command_item, buffer_name='', command_args={}):
         self.__set_locals(command_item, buffer_name, command_args)
         self.__set_functions_path()
-        function_module_name = self._function_file.replace(".py", "")
-        function_module = importlib.import_module(function_module_name)
-        function = getattr(sys.modules[function_module_name], self._function_name)
-        interpolated_input_args = self.get_interpolated_args(self._command_item)
-        if self._command_args:
-            interpolated_input_args["command_args"] = self._command_args
-        function_result = function(**interpolated_input_args)
+        self.__set_function_module_locals()
+        self.__update_command_args()
+        self.__run_function_update_variable()
+
+    def __run_function_update_variable(self):
+        function_result = self._function(**self._interpolated_input_args)
         if self._set_on_return:
             Config().get()["variables"][self._set_on_return] = function_result
+
+    def __update_command_args(self):
+        if self._command_args:
+            self._interpolated_input_args["command_args"] = self._command_args
 
     def __set_functions_path(self):
         if self._functions_path not in sys.path:
             sys.path.insert(0, self._functions_path)
+
+    def __set_function_module_locals(self):
+        self._function_module = importlib.import_module(self._function_module_name)
+        self._function = getattr(sys.modules[self._function_module_name], self._function_name)
+        self._interpolated_input_args = self.get_interpolated_args(self._command_item)
 
     def __set_locals(self, command_item, buffer_name, command_args):
         self._command_item = command_item
@@ -42,6 +54,7 @@ class run_python_function(action_base):
         self._input_args = self._command_item.get("input_args", {})
         self._set_on_return = self._command_item.get("set_on_return", None)
         self._functions_path = PathHelpers.resolve_plugin_path('functions')
+        self._function_module_name = self._function_file.replace(".py", "")
 
     def get_interpolated_args(self, command_item):
         input_args = command_item.get("input_args", {})
