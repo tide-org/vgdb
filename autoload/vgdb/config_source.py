27c06cd69275120b70830bcfef98897b27226683
Willem van Ketwich <willvk@gmail.com>
2019-02-21 18:12:54 +1100

    wip - a few things happening here

--- autoload/vgdb/config_source.py
+++ autoload/vgdb/config_source.py
@@ -1,14 +1,34 @@
 import os
+from os.path import abspath
 import path_helpers as Ph
 from yamlreader import yaml_load
 
-def __get_config_location():
+def __get_config_location_from_environment_variable():
+    config_location = os.environ.get("VGDB_CONFIG_LOCATION")
+    print("config_location: " + str(config_location))
+    if os.path.isdir(config_location):
+        return abspath(config_location)
+    base_path = Ph.get_python_scripts_base_path()
+    path_from_scripts = os.path.join(base_path, config_location)
+    if os.path.isdir(path_from_scripts):
+        return abspath(path_from_scripts)
+
+def __get_config_location_from_default_location_file():
     base_path = Ph.get_python_scripts_base_path()
     config_location_location = os.path.join(base_path, "config_location.yaml")
     location_config = yaml_load(config_location_location)
     config_location = location_config["config_location"]
-    full_config_location = os.path.join(base_path, config_location)
-    return full_config_location
+    if os.path.isdir(config_location):
+        return abspath(config_location)
+
+def __get_config_location():
+    environment_config_path = __get_config_location_from_environment_variable()
+    if environment_config_path:
+        return environment_config_path
+    location_file_path = __get_config_location_from_default_location_file()
+    if location_file_path:
+        return location_file_path
+    raise RuntimeError("error: unable to find a matching path for the config. please either set the environment variable VGDB_CONFIG_LOCATION or specify in the file config_location.yaml")
 
 def __get_all_configs():
     full_template_location = __get_config_location()
---
+++ doc/actions.md
@@ -0,0 +1,23 @@
+There are various built-in actions that can be used to do useful things with vgdb in commands.
+
+The built-in actions consist of:
+
+- create_interpolated_string
+
+    Description:
+    Fields:
+    Example:
+
+- print_debug
+- run_command
+- run_command_string
+- run_command_with_match
+- run_config_command
+- run_python_function
+- run_vim_funcion
+- set_var
+
+
+These are built-in by virtue of the fact that they are in vgdb under the ./actions path. Custom actions can also be created and specified in your config file.
+
+
